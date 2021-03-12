79f16b1089b2df46dc2fcb7da3da7496c628ed88
Zipozz <75432542+Zipozz@users.noreply.github.com>
2021-03-12 19:36:15 +0300

    Update CashbackHackServiceTest.java

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.AssertJUnit.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 
 public class CashbackHackServiceTest {
@@ -12,27 +12,27 @@ public class CashbackHackServiceTest {
     void shouldCashBack900() {
         int actual = cashbackHack.remain(900);
         int expected = 100;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     void shouldCashBack100() {
         int actual = cashbackHack.remain(100);
         int expected = 900;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     void shouldCashBack1000() {
         int actual = cashbackHack.remain(1000);
         int expected = 0;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     void shouldCashBack1100() {
         int actual = cashbackHack.remain(1100);
         int expected = 900;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
